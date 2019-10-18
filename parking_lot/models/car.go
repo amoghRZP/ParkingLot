1c94f0bbbc19d44ecc636c9ee6034df0e260c23b
amoghRZP <amogh.mishra@razorpay.com>
2019-10-18 14:47:50 +0530

    Adding basic models and main package

---
+++ parking_lot/models/car.go
@@ -0,0 +1,17 @@
+package models
+
+// car model is used for car entity  which will be inside parking lot and occupy space
+type Car struct {
+	Number string
+	Color  string
+}
+
+// Constructor for car model
+func New(number, color string) *Car {
+	return &Car{
+		Number: number,
+		Color:  color,
+	}
+}
+
+// TODO:: To add getter setter and other methods
